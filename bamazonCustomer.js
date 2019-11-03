99ddafe026ce88b8236a0ea03a13abf9ab327a44
Matt Wilson <matt@firstflightagency.com>
2019-11-03 14:07:55 -0500

    initial purchase and make another purchase functions are working, still need to get update database working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,9 +14,85 @@ connection.connect(function (err) {
     start();
 })
 
+//function to load products table, then begin purchase
 function start() {
-    connection.query("SELECT * FROM products", function (err, results) {
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, res) {
         if (err) throw err;
-        console.log("Connection Est");
+        console.table(res);
+        buyProduct();
     })
 }
+
+//function to purchase a product if stock_quantity is sufficient
+function buyProduct() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer.prompt([{
+            name: "item",
+            type: "number",
+            message: "Please select the product you would like to purchase by item_id"
+        }, {
+            name: "amount",
+            type: "number",
+            message: "How many would you like to purchase?"
+        }]).then(function (answers) {
+            console.log(answers.item)
+            var selectedItem;
+            for (var i = 0; i < results.length; i++) {
+                //console.log(results[i]);
+                if (results[i].item_id === answers.item) {
+                    selectedItem = results[i];
+                    console.log(selectedItem);
+                }
+            }
+            if (answers.amount > selectedItem.stock_quantity) {
+                console.log("We're sorry, we don't have enough " + selectedItem.product_name + " in stock to complete your order");
+                start();
+            } else {
+                var currentStock = selectedItem.stock_quantity;
+                console.log("currentStock: " +
+                    currentStock);
+                var unitsSold = answers.amount;
+                console.log("unitsSols: " + unitsSold)
+                var productSold = selectedItem.item_id;
+                console.log("productSold: " + productSold);
+                var totalPrice = answers.amount * selectedItem.price;
+                console.log("Your total cost for your order of: \n" +
+                    answers.amount + " units of " + selectedItem.product_name + " is: " + totalPrice + "\n");
+                //updateAmount();
+                restartPurchase();
+            }
+        })
+    })
+}
+
+//Function for a new purchase
+function restartPurchase() {
+    inquirer.prompt([{
+        name: "newPurchase",
+        type: "confirm",
+        message: "Would you like to make another purchase?"
+    }]).then(function (answer) {
+        if (answer.newPurchase === true) {
+            start();
+        } else {
+            console.log("Thank you for shopping with Bamazon!");
+        }
+    })
+}
+
+
+// //Function to update the amount of product in database when an item is sold
+// function updateAmount(currentStock, unitsSold, productSold) {
+//     console.log("currentStock getting passed? " + currentStock);
+//     newStock = currentStock - unitsSold;
+//     console.log(newStock);
+//     // connection.query("UPDATE products SET ? WHERE ?",
+//     //     [{
+//     //             stock_quantity: newStock
+//     //         },
+//     //         {
+//     //             item_id: productSold
+//     //         }
+//     //     ])
+// }
--- bamazonDB.sql
+++ bamazonDB.sql
@@ -14,5 +14,37 @@ CREATE TABLE products
     (80) NOT NULL,
     price DECIMAL
     (10,2) NOT NULL,
-    stock_quantity INT
-)
+    stock_quantity INT,
+    PRIMARY KEY
+    (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sekiro: Shadows Die Twice", "Video Games", 59.99, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The Royal Tenebaums", "Movies", 14.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hills ID Low Fat - case of 12", "Pet Supplies", 34.99, 32);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Spiderman - PS4", "Video Games", 59.99, 18);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dark Souls 3", "Video Games", 34.99, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Deer Antler", "Pet Supplies", 24.99, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("8 Slot Surge Protector", "Electronics", 22.49, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPad Mini", "Electronics", 299.99, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The Darjeeling Limited", "Movies", 9.99, 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("God of War", "Video Games", 39.99, 17);
